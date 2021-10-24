bab6180070dd483d4577c4ed6a018f78fea13e26
Mustafa Gemsiz <birhatgemsiz@hotmail.com>
2021-10-24 17:03:08 +0300

    finish

--- MVC-GRID/App_Start/RouteConfig.cs
+++ MVC-GRID/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MVC_GRID
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Grid", action = "List", id = UrlParameter.Optional }
             );
         }
     }
--- MVC-GRID/Controllers/GridController.cs
+++ MVC-GRID/Controllers/GridController.cs
@@ -1,4 +1,5 @@
-﻿using MVC_GRID.Models.Manager;
+﻿using MVC_GRID.Models;
+using MVC_GRID.Models.Manager;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,6 +32,65 @@ namespace MVC_GRID.Controllers
                                                 Value = i.Id.ToString()
                                             }).ToList();
             ViewBag.countryList = country;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(User model)
+        {
+            var country = db.Countries.Where(m => m.Id == model.Country.Id).FirstOrDefault();
+            model.Country = country;
+            db.Users.Add(model);
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var user = db.Users.Where(m => m.Id == id).FirstOrDefault();
+            List<SelectListItem> country = (from i in db.Countries.ToList()
+                                            select new SelectListItem
+                                            {
+                                                Text = i.Name,
+                                                Value = i.Id.ToString()
+                                            }).ToList();
+            ViewBag.countryList = country;
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(User model)
+        {
+            var user = db.Users.Where(m => m.Id == model.Id).FirstOrDefault();
+            user.Name = model.Name;
+            user.Surname = model.Surname;
+            user.Age = model.Age;
+            var country = db.Countries.Where(m => m.Id == model.Country.Id).FirstOrDefault();
+            user.Country= country;
+            db.SaveChanges();           
+            return RedirectToAction("List");
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id!=null)
+            {
+                var user = db.Users.Where(m => m.Id == id).FirstOrDefault();
+                if (user!=null)
+                {
+                    db.Users.Remove(user);
+
+                    int value = db.SaveChanges();
+                    if (value>0)
+                    {
+
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
             return View();
         }
     }
